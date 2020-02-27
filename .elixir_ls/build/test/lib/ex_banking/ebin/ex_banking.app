eea4ecbbe37984d441c47e62738661b8117266a0
Maurício Neto <malbuquerqueneto@gmail.com>
2020-02-27 17:07:20 -0300

    create specs app interface, app doesnt run yet

---
+++ .elixir_ls/build/test/lib/ex_banking/ebin/ex_banking.app
@@ -0,0 +1,6 @@
+{application,ex_banking,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"ex_banking"},
+              {modules,['Elixir.ExBanking']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -3,16 +3,39 @@ defmodule ExBanking do
   Documentation for ExBanking.
   """
 
-  @doc """
-  Hello world.
+  @type banking_error ::
+          {:error,
+           :wrong_arguments
+           | :user_already_exists
+           | :user_does_not_exist
+           | :not_enough_money
+           | :sender_does_not_exist
+           | :receiver_does_not_exist
+           | :too_many_requests_to_user
+           | :too_many_requests_to_sender
+           | :too_many_requests_to_receiver}
 
-  ## Examples
+  @spec create_user(user :: String.t()) :: :ok | banking_error
+  defdelegate create_user(user), to:
 
-      iex> ExBanking.hello()
-      :world
+  @spec deposit(user :: String.t(), amount :: number, currency :: String.t()) ::
+          {:ok, new_balance :: number} | banking_error
+  defdelegate deposit(user, amount, currency), to:
 
-  """
-  def hello do
-    :world
-  end
+  @spec withdraw(user :: String.t(), amount :: number, currency :: String.t()) ::
+          {:ok, new_balance :: number} | banking_error
+  defdelegate withdraw(user, amount, currency), to:
+
+  @spec get_balance(user :: String.t(), currency :: String.t()) ::
+          {:ok, balance :: number} | banking_error
+  defdelegate get_balance(user, currency), to:
+
+  @spec send(
+          from_user :: String.t(),
+          to_user :: String.t(),
+          amount :: number,
+          currency :: String.t()
+        ) :: {:ok, from_user_balance :: number, to_user_balance :: number} | banking_error
+  defdelegate(from_user, to_user, amount, currency), to:
+
 end
